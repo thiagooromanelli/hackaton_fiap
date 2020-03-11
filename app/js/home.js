7189ef0d8c1ef8d9e6089e7467737a179911230d
Murillo Cuervo Tarôuco <murillocuervot@gmail.com>
2020-03-10 21:30:58 -0300

    roleta

--- app/js/home.js
+++ app/js/home.js
@@ -5,3 +5,204 @@ $(function () {
       $('.offcanvas-collapse').toggleClass('open')
     })
   })
+  
+  
+function myFunction() {
+
+    var vid = document.getElementById("myAudio");
+    vid.currentTime = 0
+  
+    vid.play();
+
+    setTimeout(function(){
+    document.getElementById("terceiro").classList.remove('active');
+    document.getElementById("primeira").classList.remove('active');
+    document.getElementById("segundo").classList.add('active');
+    setTimeout(function(){
+      document.getElementById("segundo").classList.remove('active');
+      document.getElementById("primeira").classList.remove('active');
+      document.getElementById("terceiro").classList.add('active');
+      setTimeout(function(){
+        document.getElementById("terceiro").classList.remove('active');
+        document.getElementById("segundo").classList.remove('active');
+        document.getElementById("primeira").classList.add('active');  
+        setTimeout(function(){
+          document.getElementById("primeira").classList.remove('active');
+          document.getElementById("terceiro").classList.remove('active');
+          document.getElementById("segundo").classList.add('active');
+          setTimeout(function(){
+            document.getElementById("segundo").classList.remove('active');
+            document.getElementById("primeira").classList.remove('active')
+            document.getElementById("terceiro").classList.add('active');
+            setTimeout(function(){
+              document.getElementById("terceiro").classList.remove('active');
+              document.getElementById("segundo").classList.remove('active')
+              document.getElementById("primeira").classList.add('active');  
+              setTimeout(function(){
+                document.getElementById("primeira").classList.remove('active');
+                document.getElementById("terceiro").classList.remove('active');
+                document.getElementById("segundo").classList.add('active');
+                setTimeout(function(){
+                  document.getElementById("segundo").classList.remove('active');
+                  document.getElementById("primeira").classList.remove('active');
+                  document.getElementById("terceiro").classList.add('active');
+                  setTimeout(function(){
+                    document.getElementById("terceiro").classList.remove('active');
+                    document.getElementById("segundo").classList.remove('active');
+                    document.getElementById("primeira").classList.add('active');  
+                    setTimeout(function(){
+                      document.getElementById("primeira").classList.remove('active');
+                      document.getElementById("terceiro").classList.remove('active');
+                      document.getElementById("segundo").classList.add('active');
+                      setTimeout(function(){
+                        document.getElementById("segundo").classList.remove('active');
+                        document.getElementById("primeira").classList.remove('active');
+                        document.getElementById("terceiro").classList.add('active');
+                        setTimeout(function(){
+                          document.getElementById("terceiro").classList.remove('active');
+                          document.getElementById("segundo").classList.remove('active');
+                          document.getElementById("primeira").classList.add('active');  
+                          setTimeout(function(){
+                            document.getElementById("primeira").classList.remove('active');
+                            document.getElementById("terceiro").classList.remove('active');
+                            document.getElementById("segundo").classList.add('active');
+                            setTimeout(function(){
+                              document.getElementById("segundo").classList.remove('active');
+                              document.getElementById("primeira").classList.remove('active');
+                              document.getElementById("terceiro").classList.add('active');
+                              setTimeout(function(){
+                                document.getElementById("terceiro").classList.remove('active');
+                                document.getElementById("segundo").classList.remove('active');
+                                document.getElementById("primeira").classList.add('active'); 
+                                setTimeout(function(){
+                                  document.getElementById("primeira").classList.remove('active');
+                                  document.getElementById("terceiro").classList.remove('active');
+                                  document.getElementById("segundo").classList.add('active');
+                                  setTimeout(function(){
+                                    document.getElementById("segundo").classList.remove('active');
+                                    document.getElementById("primeira").classList.remove('active');
+                                    document.getElementById("terceiro").classList.add('active');
+                                    setTimeout(function(){
+                                      document.getElementById("terceiro").classList.remove('active');
+                                      document.getElementById("segundo").classList.remove('active');
+                                      document.getElementById("primeira").classList.add('active');  
+                                      setTimeout(function(){
+                                        document.getElementById("primeira").classList.remove('active');
+                                        document.getElementById("terceiro").classList.remove('active');
+                                        document.getElementById("segundo").classList.add('active');
+                                        setTimeout(function(){
+                                          document.getElementById("segundo").classList.remove('active');
+                                          document.getElementById("primeira").classList.remove('active');
+                                          document.getElementById("terceiro").classList.add('active');
+                                          setTimeout(function(){
+                                            document.getElementById("terceiro").classList.remove('active');
+                                            document.getElementById("segundo").classList.remove('active');
+                                            document.getElementById("primeira").classList.add('active');  
+                                            setTimeout(function(){
+                                              document.getElementById("primeira").classList.remove('active');
+                                              document.getElementById("terceiro").classList.remove('active');
+                                              document.getElementById("segundo").classList.add('active');
+                                              setTimeout(function(){
+                                                document.getElementById("segundo").classList.remove('active');
+                                                document.getElementById("primeira").classList.remove('active');
+                                                document.getElementById("terceiro").classList.add('active');
+                                                setTimeout(function(){
+                                                  document.getElementById("terceiro").classList.remove('active');
+                                                  document.getElementById("segundo").classList.remove('active');
+                                                  document.getElementById("primeira").classList.add('active');  
+                                                  setTimeout(function(){
+                                                    document.getElementById("primeira").classList.remove('active');
+                                                    document.getElementById("terceiro").classList.remove('active');
+                                                    document.getElementById("segundo").classList.add('active');
+                                                    setTimeout(function(){
+                                                      document.getElementById("segundo").classList.remove('active');
+                                                      document.getElementById("primeira").classList.remove('active');
+                                                      document.getElementById("terceiro").classList.add('active');
+                                                      setTimeout(function(){
+                                                        document.getElementById("terceiro").classList.remove('active');
+                                                        document.getElementById("segundo").classList.remove('active');
+                                                        document.getElementById("primeira").classList.add('active');  
+                                                        setTimeout(function(){
+                                                          document.getElementById("primeira").classList.remove('active');
+                                                          document.getElementById("terceiro").classList.remove('active');
+                                                          document.getElementById("segundo").classList.add('active');
+                                                          setTimeout(function(){
+                                                            document.getElementById("segundo").classList.remove('active');
+                                                            document.getElementById("primeira").classList.remove('active');
+                                                            document.getElementById("terceiro").classList.add('active');
+                                                            setTimeout(function(){
+                                                              document.getElementById("terceiro").classList.remove('active');
+                                                              document.getElementById("segundo").classList.remove('active');
+                                                              document.getElementById("primeira").classList.add('active');  
+                                                              setTimeout(function(){
+                                                                document.getElementById("primeira").classList.remove('active');
+                                                                document.getElementById("terceiro").classList.remove('active');
+                                                                document.getElementById("segundo").classList.add('active');
+                                                                setTimeout(function(){
+                                                                  document.getElementById("segundo").classList.remove('active');
+                                                                  document.getElementById("primeira").classList.remove('active');
+                                                                  document.getElementById("terceiro").classList.add('active');
+                                                                  setTimeout(function(){
+                                                                    document.getElementById("terceiro").classList.remove('active');
+                                                                    document.getElementById("segundo").classList.remove('active');
+                                                                    document.getElementById("primeira").classList.add('active');  
+                                                                    setTimeout(function(){
+                                                                      document.getElementById("primeira").classList.remove('active');
+                                                                      document.getElementById("terceiro").classList.remove('active');
+                                                                      document.getElementById("segundo").classList.add('active');
+                                                                      setTimeout(function(){
+                                                                        document.getElementById("segundo").classList.remove('active');
+                                                                        document.getElementById("primeira").classList.remove('active');
+                                                                        document.getElementById("terceiro").classList.remove('active');
+                                                                        var aleatorio = Math.floor(Math.random() * 3);
+                                                                        var imagem;
+                                                                        if(aleatorio == 0){
+                                                                          imagem = 'primeira';
+                                                                        }
+                                                                        if(aleatorio == 1){
+                                                                          imagem = 'segundo';
+                                                                        }
+                                                                        if(aleatorio == 2){
+                                                                          imagem = 'terceiro';
+                                                                        }
+                                                                        
+                                                                        document.getElementById(imagem).classList.add('active');
+                                                                        setTimeout(function(){                                                                          
+                                                                          
+                                                                        }, 1000);
+                                                                      }, 400);
+                                                                    }, 400);                                                                    
+                                                                  }, 400);
+                                                                }, 400);
+                                                              }, 400);
+                                                            }, 300);
+                                                          }, 300);
+                                                        }, 300);
+                                                      }, 200);
+                                                    }, 200);
+                                                  }, 200);
+                                                }, 100);
+                                              }, 100);
+                                            }, 100);
+                                          }, 100);
+                                        }, 100);
+                                      }, 100);
+                                    }, 100);
+                                  }, 100);
+                                }, 100); 
+                              }, 100);
+                            }, 100);
+                          }, 100);
+                        }, 200);
+                      }, 200);
+                    }, 200);
+                  }, 300);
+                }, 300);
+              }, 300);
+            }, 400);
+          }, 400);
+        }, 400);
+      }, 500);
+    }, 500);
+  }, 500);
+}
